d7ccc6990e8bab1f19bfd9a6cbd8685a60004bce
Jacob Kagon <jacob@kagon.net>
2020-09-14 23:21:39 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,8 +1,13 @@
 scale = [2,3,4,5]
 
 def find_max_value(array)
-  
-  while 
-  
-
+  cout = 0
+  max_value = -1
+  while count < array.length do
+    if max_value < array[count]
+      max value = array[count]
+    end
+    count += 1
+  end
+  max_value
 end
